eeebfaf97cee91bfedf2829a0d10ad9e882bedae
magehui <magejump@gmail.comm>
2018-05-10 08:53:02 +0800

    2018.5.10

---
+++ GITCSDN2D/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    private WeponScript[]  weapons;
+
+    void Awake()
+    {
+        weapons = GetComponentInChildren<WeponScript[]>();
+    }
+
+     void Update()
+    {
+        foreach (WeponScript weapon in weapons)
+        {
+            if (weapon != null&&weapon.canAttack)
+            {
+                weapon.Attack(true);
+            }
+        }
+    }
+
+
+
+}
